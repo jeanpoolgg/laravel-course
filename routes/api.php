3090b2492bb0ad3d60c81077dc9b1da5a10c36a0
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-09-12 03:12:55 -0500

    Creacion de Controller

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\BackendController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get("/test", function(){
     return "El backend funciona correctamente";
 });
+
+Route::get("/backend", [BackendController::class, "get"]);
