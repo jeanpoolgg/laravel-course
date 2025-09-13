0704f4fdb745044684c2c63dace5a99851ab4806
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-09-13 17:27:23 -0500

    Datos rellenables en Models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $table = "product";
+
+    protected $fillable = ["name", "description", "price", "category_id"];
 }
